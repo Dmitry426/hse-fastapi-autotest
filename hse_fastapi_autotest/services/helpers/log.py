2936f02458e72d7d2807f5a74d9c268f0af81b6e
Dmitry <kachkindmitry@gmail.com>
2024-02-26 00:07:01 +0300

    Chore : small project fixes

---
+++ hse_fastapi_autotest/services/helpers/log.py
@@ -0,0 +1,29 @@
+import logging.config
+
+
+def setup_logger_config(logger_name, log_level=logging.INFO):
+    return {
+        'version': 1,
+        'formatters': {
+            'default': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+            },
+        },
+        'loggers': {
+            logger_name: {
+                'level': log_level,
+                'handlers': ['console'],
+                'propagate': False,
+            },
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
--- hse_fastapi_autotest/services/testing_services.py
+++ hse_fastapi_autotest/services/testing_services.py
@@ -7,10 +7,13 @@ import os
 import subprocess
 from pathlib import Path
 from typing import List
+from logging import config as logging_config
 
 from hse_fastapi_autotest import PROJECT_ROOT
+from hse_fastapi_autotest.services.helpers.log import setup_logger_config
 
-logger = logging.getLogger(__name__)
+logging_config.dictConfig(setup_logger_config("fastapi_autotest"))
+logger = logging.getLogger("fastapi_autotest")
 
 
 def run_pytest(repo_url: str, project_destination: Path, html_output_dir: Path):
@@ -35,7 +38,7 @@ def run_pytest(repo_url: str, project_destination: Path, html_output_dir: Path):
         "--self-contained-html",
     ]
     result = subprocess.run(pytest_args, check=False)
-    print(result.stdout)
+    logger.info(result.stdout)
 
 
 def flake8_html_report(
@@ -73,7 +76,7 @@ def flake8_html_report(
             )
 
     except subprocess.CalledProcessError as e:
-        print(f"Error running flake8: {e}")
+        logger.error(f"Error running flake8: {e}")
 
 
 def pylint_json_report(
@@ -98,25 +101,25 @@ def pylint_json_report(
             pylint_command, capture_output=True, text=True, check=False
         )
 
-        print(pylint_process.stdout)
-
         if pylint_process.returncode == 0:
-            print("pylint passed without any issues.")
+            logger.info("pylint passed without any issues.")
         else:
             try:
                 json_output = json.loads(pylint_process.stdout)
+                logger.info(json_output)
             except json.JSONDecodeError:
-                print("Error: pylint did not produce valid JSON output.")
+                logger.error("Error: pylint did not produce valid JSON output.")
                 return
 
             json_report_path = output_directory / "report.json"
             with open(json_report_path, "w", encoding="utf-8") as json_file:
                 json.dump(json_output, json_file, indent=2)
 
-            print(f"pylint found issues. JSON report saved to '{json_report_path}'")
+            logger.error(
+                f"pylint found issues. JSON report saved to '{json_report_path}'")
 
     except subprocess.CalledProcessError as e:
-        print(f"Error running pylint: {e}")
+        logger.error(f"Error running pylint: {e}")
 
 
 def report_junk_folders(
@@ -150,9 +153,9 @@ def report_junk_folders(
                 for report_line in junk_report:
                     report_file.write(report_line + "\n")
 
-            print(f"Junk report saved to '{report_file_path}'")
+            logger.info(f"Junk report saved to '{report_file_path}'")
         else:
-            print("No junk folders found in the repository.")
+            logger.info("No junk folders found in the repository.")
 
     except subprocess.CalledProcessError as e:
-        print(f"Error checking for useless directories : {e}")
+        logger.error(f"Error checking for useless directories : {e}")
